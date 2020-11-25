c0ace65bada7698c3aa987bb68b0894e6b95d81f
HectorMiranda <hector.miranda8@upr.edu>
2020-11-24 20:57:25 -0400

    Added support for newegg stock tracking in combos

--- src/main.py
+++ src/main.py
@@ -45,11 +45,31 @@ async def check_url_newegg_single(url):
 
 
 async def check_url_newegg_combo(url):
-    print()
+    page = requests.get(url, headers=firefox_header)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # print(soup)
+    print("\nProduct: " + str(newegg_name_combo(soup)))
+    instock = newegg_instock_combo(str(soup))
+    if instock:
+        text = colored("In stock: " + str(instock), 'green')
+        print(text)
+        price = str(newegg_price_combo(soup))
+        print("Price: " + price)
+        avail_stock_firefox.append(url)
+    else:
+        text = colored("In stock: " + str(instock), 'red')
+        print(text)
 
 
 def newegg_instock(soup):
     display = "OUT OF STOCK"
+    if soup.find(display) != -1:
+        return False
+    return True
+
+
+def newegg_instock_combo(soup):
+    display = "class=\"note\">OUT OF STOCK"
     if soup.find(display) != -1:
         return False
     return True
@@ -61,15 +81,34 @@ def newegg_price(soup):
     return "$" + price[0]
 
 
+def newegg_price_combo(soup):
+    data = str(soup.find(class_="current")).split("\n")
+    price = re.findall(r"\d+\.\d+", data[0])
+    # print(data)
+    # print(price)
+    return "$" + price[0]
+
+
 def newegg_name(soup):
     unformat_name = str(soup.find(class_="name")).split(">")
     return unformat_name[1].split("<")[0]
 
 
+def newegg_name_combo(soup):
+    data = str(soup.find(class_="wrapper")).split("\n")
+    line = re.findall(r"\"name\">.*</span", data[1])
+    name = re.findall(r">.*<", line[0])
+    size = len(name[0])
+    name = name[0][1:size-1]
+    # print(name)
+    return name
+
 async def check_newegg():
     tasks = []
     for url in URL_NEWEGG:
         tasks.append(asyncio.ensure_future(check_url_newegg_single(url)))
+    for url in URL_NEWEGG_COMBOS:
+        tasks.append(asyncio.ensure_future(check_url_newegg_combo(url)))
     await asyncio.gather(*tasks)
 
 
@@ -78,6 +117,10 @@ def add_urls():
     for url in file:
         if url.startswith("https"):
             URL_NEWEGG.append(url)
+    file = open("../urls/urls_newegg_combos.txt", "r")
+    for url in file:
+        if url.startswith("https"):
+            URL_NEWEGG_COMBOS.append(url)
 
 
 def main():
@@ -90,8 +133,8 @@ def main():
         finally:
             end = time.time() - start
             print("\nTime to execute: %.4f" % end, "secs\n")
-            # for url in avail_stock_firefox:
-            #     webbrowser.get('firefox').open(url)
+            for url in avail_stock_firefox:
+                webbrowser.get('firefox').open(url)
             # for url in avail_stock_chrome:
             #     webbrowser.get('chrome').open(url)
             close = False
@@ -105,6 +148,7 @@ def main():
             time_to_wait = random.randint(10, 30)
             print("\nTime to refresh or close: " + str(time_to_wait) + " seconds")
             time.sleep(time_to_wait)
+            #close = True
             if close:
                 print("Closing bot.")
                 sys.exit(0)
